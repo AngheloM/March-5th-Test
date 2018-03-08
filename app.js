f94f6322ffc9f6c04a7257134ecb2ded65c00a6b
AngheloM <macha.anghelo@gmail.com>
2018-03-07 14:38:29 -1000

    end

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var array = []
+var array = [
+  "Toyota",
+  "Nissan",
+  "Honda",
+  "Kia"
+];
+
+for (var i = 0; i < 1; i++) {
+  console.log(array[i])
+}
+
+console.log(array)
+
+var string = "I love coding!";
+
+console.log(string)
+
 
